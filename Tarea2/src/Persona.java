64045fa4da9287ba7ed4725f6593f6293d714bd4
Emily-allende <79849579+Emily-allende@users.noreply.github.com>
2022-05-10 10:07:25 -0300

    Update Persona.java

--- Tarea2/src/Persona.java
+++ Tarea2/src/Persona.java
@@ -1,3 +1,11 @@
 public class Persona {
-
+   String nombre;
+   int ci;
+   int deuda;
+  
+  public persona(String nombre, int ci, int deuda){
+    this.nombre=nombre;
+    this.ci=ci;
+    this.deuda=deuda;
+  }
 }
