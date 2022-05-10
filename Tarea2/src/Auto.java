e6f6e6df7959229b2c396c1cae77d1dcac722d30
camilodanielli404 <82782043+camilodanielli404@users.noreply.github.com>
2022-05-10 10:07:27 -0300

    Update Auto.java

--- Tarea2/src/Auto.java
+++ Tarea2/src/Auto.java
@@ -1,2 +1,16 @@
+import persona
+
 public class Auto {
+  persona dueño;
+  static String marca;
+  int matricula;
+  int tiempoT;
+  
+  public auto(persona dueño, String marca, int matricula){
+      this.dueño = dueño;
+      this.marca = marca;
+      this.matricula = matricula
+      tiempoT = 0;
+  }
 }
+
