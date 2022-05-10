7e678263d3e64b650e1ca8978aea5b0fa4cf6405
Emily-allende <79849579+Emily-allende@users.noreply.github.com>
2022-05-10 10:20:07 -0300

    Create Estacionamiento.java

---
+++ Tarea2/src/Estacionamiento.java
@@ -0,0 +1,10 @@
+import Auto.java
+public class Estacionamiento{
+  int catidad;
+  boolean espacio;
+  auto Auto;
+  
+  public estacionamiento(int cantida, boolean espacio, auto Auto){}
+  
+  
+}
